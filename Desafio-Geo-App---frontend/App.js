502cac37c45b2a01e6d20501ec2b44c17479ae3d
Matheuzferreira <protimatheus@gmail.com>
2025-11-27 00:15:16 -0300

    Comeco

---
+++ Desafio-Geo-App---frontend/App.js
@@ -0,0 +1,20 @@
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CreateReportScreen from './src/CreateReport'; 
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="CreateReport">
+        <Stack.Screen 
+          name="CreateReport" 
+          component={CreateReportScreen} 
+          options={{ title: 'Criar Relatório' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Desafio-Geo-App---frontend/src/CreateReport.js
@@ -0,0 +1,64 @@
+
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import api from './services/api'; 
+
+const CreateReportScreen = ({ navigation }) => {
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('');
+
+  const handleSubmit = async () => {
+    if (!titulo || !descricao) {
+      Alert.alert('Atenção', 'Preencha Título e Descrição.');
+      return;
+    }
+    
+    try {
+      const reportData = {
+        titulo,
+        descricao,
+        local: 'Local Capturado', 
+        foto: 'URL_TEMPORARIA',
+        dataHora: new Date(),
+        laboratorio: 'Lab Geo'
+      };
+
+      await api.post('/reports', reportData);
+
+      Alert.alert('Sucesso', 'Relatório enviado ao servidor!');
+      setTitulo(''); 
+      setDescricao('');
+    } catch (error) {
+      console.error(error.response?.data || error);
+      Alert.alert('Erro', 'Falha ao enviar. Verifique o IP e o Backend.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Novo Relatório</Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder="Título" 
+        value={titulo} 
+        onChangeText={setTitulo} 
+      />
+      <TextInput 
+        style={styles.input} 
+        placeholder="Descrição" 
+        value={descricao} 
+        onChangeText={setDescricao} 
+        multiline
+      />
+      <Button title="Enviar para o Backend" onPress={handleSubmit} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 },
+  title: { fontSize: 24, marginBottom: 20 },
+  input: { borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 10 },
+});
+
+export default CreateReportScreen;
